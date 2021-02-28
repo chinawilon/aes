d7a0fa132c94346ffc1392d2d1fde7229bc11fc3
wuwenlong <wuwenlong@itowan.com>
2021-02-28 10:08:02 +0800

    add the testCase

--- tests/AESTest.php
+++ tests/AESTest.php
@@ -12,9 +12,6 @@ class AESTest extends TestCase
         $key = "abc132123123123e425ab234523a45ef";
 
         $aes = new AES($key);
-        echo 'encrypt:'$encrpyt = $aes->encrypt($plain);
-        echo $aes->decrypt($encrpyt);
-
         $this->assertEquals(
             $aes->decrypt($aes->encrypt($plain)),
             $plain
